3757bf372eaa9a2e15ef4566d076da80a993fdfd
Ogaremma <emmawills725@gmail.com>
2025-07-15 15:42:30 +0100

    Initial commit excluding To do list Project folder

---
+++ prom.js
@@ -0,0 +1,81 @@
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+        setTimeout(() => {
+            if (rand < 0.7) {
+                resolve('YOUR FAKE DATA HERE');
+            }
+            reject('Request Error!');
+        }, 1000)
+    })
+}
+
+fakeRequest('/dogs/1')
+    .then((data) => {
+        console.log("DONE WITH REQUEST!")
+        console.log('data is:', data)
+    })
+    .catch((err) => {
+        console.log("OH NO!", err)
+    })
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+}
+
+
+const login = (username, password) => {
+    if (!username || !password) throw 'Missing Credentials';
+    if (password === 'cocktail') return 'Welcome!';
+    throw 'Invalid Password';
+}
+
+login('user', 'cocktail')
+    .then((msg) => {
+        console.log("Logged in successfully:", msg);
+    })
+    .catch((err) => {
+        console.log("Login failed:", err);
+    });
+
+
+
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// delayedColorChange('red', 1000, () => {
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+//                 delayedColorChange('blue', 1000, () => {
+//                     delayedColorChange('indigo', 1000, () => {
+//                         delayedColorChange('violet', 1000, () => {
+
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// });
+
+
